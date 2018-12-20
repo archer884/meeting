19f9a174e44c71937ff41d162860f671aee55b11
J/A <jarcher@balglobal.com>
2018-12-20 14:22:28 -0600

    Improve cli, timezone handling
    CLI now accepts improved list commands.
    
    Logging dates initially as UTC causes a problem as a result of the user
    entering a non-UTC time for the meeting. (Example: 12 US Central is 18
    UTC). Logging dates are now initialized as local time and then converted
    to UTC for storage.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use chrono::{Date, DateTime, NaiveDate, Utc};
+use chrono::{Date, DateTime, Local, NaiveDate, Utc};
 use serde::{Deserialize, Serialize};
 use std::fmt::{self, Display};
+use std::fs;
 use std::path::PathBuf;
 use std::{error, result};
 use structopt::StructOpt;
@@ -20,13 +21,17 @@ enum Opt {
         length: u32,
     },
 
-    // Format is 2018-12-19
+    /// List meetings within a given range
+    ///
+    /// Provide a start and end date to list all meetings occurring within that date range.
+    /// A single date will list only meetings occurring on that day. Providing no dates at all
+    /// will list only meetings for the current day.
     #[structopt(name = "list", about = "List meetings for a given timeframe")]
     List {
-        /// Beginning of date range, e.g. 2018-12-1
-        start: NaiveDate,
-        /// End of date range, e.g. 2018-12-31
-        end: NaiveDate,
+        /// e.g. 2018-12-31
+        start: Option<NaiveDate>,
+        /// e.g. 2018-12-31
+        end: Option<NaiveDate>,
     },
 }
 
@@ -39,22 +44,24 @@ struct Meeting {
 impl Meeting {
     fn today(start: u32, length: u32) -> Meeting {
         Meeting {
-            timestamp: Utc::today().and_hms(start, 0, 0),
+            timestamp: Local::today().and_hms(start, 0, 0).with_timezone(&Utc),
             length,
         }
     }
 
-    fn is_within_range(&self, start: Date<Utc>, end: Date<Utc>) -> bool {
-        self.timestamp.date() >= start && self.timestamp.date() <= end
+    fn is_within_range(&self, start: Date<Local>, end: Date<Local>) -> bool {
+        let compare = self.timestamp.with_timezone(&Local).date();
+        compare >= start && compare <= end
     }
 }
 
 impl Display for Meeting {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let local = self.timestamp.with_timezone(&Local);
         write!(
             f,
             "{}: {} minutes",
-            self.timestamp.format("%F %R"),
+            local.format("%F %R"),
             self.length
         )
     }
@@ -89,33 +96,64 @@ fn log(start: u32, length: u32) -> AppResult<()> {
     Ok(())
 }
 
-fn list(start: NaiveDate, end: NaiveDate) -> AppResult<()> {
-    use std::fs;
+fn list(start: Option<NaiveDate>, end: Option<NaiveDate>) -> AppResult<()> {
+    match (start, end) {
+        (Some(start), Some(end)) => list_range(start, end),
+        (Some(start), None) => list_date(start),
+        (None, None) => list_today(),
 
-    fn parse(s: &str) -> Option<Meeting> {
-        serde_json::from_str(s).ok()
+        _ => unreachable!(
+            "Structopt should not fill the second positional argument before the first."
+        ),
     }
+}
 
-    let start = to_utc_date(start)?;
-    let end = to_utc_date(end)?;
+fn list_range(start: NaiveDate, end: NaiveDate) -> AppResult<()> {
+    let start = to_local_date(start)?;
+    let end = to_local_date(end)?;
+    let records = load_records(|x| x.is_within_range(start, end))?;
+    print_records(records);
+    Ok(())
+}
+
+fn list_date(date: NaiveDate) -> AppResult<()> {
+    let date = to_local_date(date)?;
+    let records = load_records(|x| x.is_within_range(date, date))?;
+    print_records(records);
+    Ok(())
+}
+
+fn list_today() -> AppResult<()> {
+    let date = Local::today();
+    let records = load_records(|x| x.is_within_range(date, date))?;
+    print_records(records);
+    Ok(())
+}
+
+fn load_records(f: impl FnMut(&Meeting) -> bool) -> AppResult<Vec<Meeting>> {
     let content = fs::read_to_string(&log_path()?)?;
     let mut records: Vec<_> = content
         .lines()
-        .filter_map(parse)
-        .filter(|x| x.is_within_range(start, end))
+        .filter_map(parse_meeting)
+        .filter(f)
         .collect();
 
     records.sort_by_key(|x| x.timestamp);
+    Ok(records)
+}
 
+fn print_records(records: impl IntoIterator<Item = Meeting>) {
     let mut minutes = 0;
-    for record in &records {
+    for record in records {
         minutes += record.length;
         println!("{}", record);
     }
 
     println!("Total hours: {:.1}", f64::from(minutes) / 60.0);
+}
 
-    Ok(())
+fn parse_meeting(s: &str) -> Option<Meeting> {
+    serde_json::from_str(s).ok()
 }
 
 fn log_path() -> AppResult<PathBuf> {
@@ -129,12 +167,12 @@ fn log_path() -> AppResult<PathBuf> {
     Ok(path)
 }
 
-fn to_utc_date(date: NaiveDate) -> AppResult<Date<Utc>> {
+fn to_local_date(date: NaiveDate) -> AppResult<Date<Local>> {
     use chrono::offset::{LocalResult, TimeZone};
     use std::cmp;
 
     // Genesis, chapter 3.
-    match Utc.from_local_date(&date) {
+    match Local.from_local_date(&date) {
         LocalResult::None => Err("Invalid date")?,
         LocalResult::Single(date) => Ok(date),
         LocalResult::Ambiguous(left, right) => Ok(cmp::min(left, right)),
